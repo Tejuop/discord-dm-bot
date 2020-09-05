94e3b1919cd88af49c6d595971fec7b44c88c08c
redmemz <redmemz@hitler.rocks>
2020-09-05 17:29:07 +0200

    Create bot.py
    How to use: 
    python bot.py
    
    Be sure to have Python 3.6

---
+++ bot.py
@@ -0,0 +1,59 @@
+import discord
+import asyncio
+from discord.ext import commands
+
+token = '<Your bot's token!>'
+bot = commands.Bot(command_prefix='kk!') # Setting bot prefix to 'kk!'
+
+# Send one dm!
+# Usage: 'kk!dm 0000000000000000 My Message To You!!'
+@bot.command()
+async def dm(ctx, userid: int,*, dm: str):
+    boi = bot.get_user(userid)
+    iq = 0
+    if iq < 69:
+        await boi.send(dm)
+    await ctx.send('DM send!')
+
+# Send endless dm's! 
+# Usage: 'kk!spam 00000000000000000 My Reapeated Message To You!!'
+@bot.command()
+async def spam(ctx, userid: int,*, dm: str):
+    iq = 0
+    while iq < 69:
+        boi = bot.get_user(userid)
+        if iq < 5:
+            await boi.send(dm)
+        await ctx.send('DM send!')
+
+# Help!!!!
+# Usage: 'kk!nani'
+@bot.command()
+async def nani(ctx):
+    embed = discord.Embed(
+        title="HELP! U.u",
+        color=discord.Colour(0x3b12ef),
+        description="Commands:",
+    )
+    embed.add_field(
+        name="kk!dm",
+        value='Send a DM! (kk!dm userid msg)'
+    )
+    embed.add_field(
+        name="kk!spam",
+        value="Spam in DM's! (kk!spam userid msg)"
+    )
+    embed.add_field(
+        name="kk!nani",
+        value="Help!"
+    )
+    await ctx.send(
+        embed=embed
+    )
+
+@bot.event
+async def on_ready():
+    name, id = bot.user.name, bot.user.id
+    print('------\nLogged in with:\nUSER:%s\nID:%s\n------' % name, id))
+
+bot.run(token)
